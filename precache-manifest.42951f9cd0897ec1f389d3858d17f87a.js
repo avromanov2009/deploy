c8ace683f5364589905ed8ebadee0831fd8b7afe
Andrei Romanov <romanov.andrei.v@gmail.com>
2020-02-09 19:59:07 +0300

    Updates

---
+++ precache-manifest.42951f9cd0897ec1f389d3858d17f87a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c6652a100e747241b45662cd2f22272",
+    "url": "/avromanov2009/deploy/index.html"
+  },
+  {
+    "revision": "0f293ce64e7bc19105a8",
+    "url": "/avromanov2009/deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "149574af6e00d241bbc4",
+    "url": "/avromanov2009/deploy/static/js/2.ae69f4c5.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/avromanov2009/deploy/static/js/2.ae69f4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f293ce64e7bc19105a8",
+    "url": "/avromanov2009/deploy/static/js/main.bacd6800.chunk.js"
+  },
+  {
+    "revision": "42a8f5927cf64774e5ac",
+    "url": "/avromanov2009/deploy/static/js/runtime-main.7d41b2c1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/avromanov2009/deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
