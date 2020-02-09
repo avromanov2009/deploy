3edba259f8b4ced9ed13280f204e62d1a5cb78cf
Andrei Romanov <romanov.andrei.v@gmail.com>
2020-02-09 19:58:47 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Simple deployment example on GitHub Pages</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
